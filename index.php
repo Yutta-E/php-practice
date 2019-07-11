131e1606e09f55177508c9d60ff6b6d5f5326236
Yutta-E <wasekyo-u-8@fuji.waseda.jp>
2019-07-11 15:20:56 +0900

    index.phpをpost2配下に作成

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
+    //ログインしている
+    $SESSION['time'] = time();
+    $member = $db->prepare('SELECT * FROM members WHERE id=?');
+    $members->execute(array($_SESSION['id']));
+    $member = $members->fetch();
+} else {
+    //ログインしていない
+    header('Location: login.php'); exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ひとこと掲示板</title>
+
+	<link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+    <form action="" method="post">
+    <dl>
+        <dt>メッセージをどうぞ</dt>
+        <dd>
+            <textarea name="message" cols="50" rows="5"></textarea>
+        </dd>
+    </dl> 
+    <div>
+        <input type="submit" value="投稿する">
+    </div>   
+
+    </form>
+
+</body>
+</html>
